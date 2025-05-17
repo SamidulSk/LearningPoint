1ce0acc156508064ffbf8bccfac268b515fef7e0
SamidulSk <samidulsk@@>
2025-05-17 06:17:10 +0530

    File sensitivity error

--- Backend/utils/appError.js
+++ Backend/utils/appError.js
@@ -1,11 +1,1 @@
-class AppError extends Error { // AppError.js
-    constructor(message, statusCode) {
-      super(message);
-  
-      this.statusCode = statusCode;
-  
-      Error.captureStackTrace(this, this.constructor);
-    }
-  }
-  
-  export default AppError;
+//let me in
