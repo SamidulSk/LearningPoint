a87a31b5b622b361a4f590d21c339b900122d867
SamidulSk <samidulsk@@>
2025-05-17 08:36:55 +0530

    cors error fixed

--- Frontend/src/Helpers/axiosInstance.js
+++ Frontend/src/Helpers/axiosInstance.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:4000/api/v1"
@@ -7,3 +9,5 @@ const axiosInstance = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
 });
+
+export default axiosInstance;
