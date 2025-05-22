eefb32b02b15e08f363f460f12d6904d4a93e010
SamidulSk <samidulsk@@>
2025-05-22 20:57:54 +0530

    update frontend

--- Frontend/src/Pages/Course/CourseList.jsx
+++ Frontend/src/Pages/Course/CourseList.jsx
@@ -23,7 +23,7 @@ function CourseList() {
             <div className="min-h-[90vh] pt-12 pl-20 flex flex-col gap-10 text-white">
                 <h1 className="text-center text-3xl font-semibold mb-5">
                     Explore the courses made by  
-                    <span className="font-bold text-yellow-500">
+                    <span className="font-bold text-yellow-500 p-2">
                          SAMIDUL
                     </span>
                 </h1>
