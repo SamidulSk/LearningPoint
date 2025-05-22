ffc99660b93c1325f41131fe1ee9954c30911ee3
SamidulSk <samidulsk@@>
2025-05-22 20:55:37 +0530

    UPDATE FRONTEND

--- Frontend/src/Pages/Course/CourseList.jsx
+++ Frontend/src/Pages/Course/CourseList.jsx
@@ -22,9 +22,9 @@ function CourseList() {
         <HomeLayout>
             <div className="min-h-[90vh] pt-12 pl-20 flex flex-col gap-10 text-white">
                 <h1 className="text-center text-3xl font-semibold mb-5">
-                    Explore the courses made by
+                    Explore the courses made by  
                     <span className="font-bold text-yellow-500">
-                        SAMIDUL
+                         SAMIDUL
                     </span>
                 </h1>
                 <div className="mb-10 flex flex-wrap gap-14">
